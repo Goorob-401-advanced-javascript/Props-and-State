b8d872e4428a36ecba6ddf12296fc1ba43aed908
Goorob <runner@fv-az59.k4ee0zmrwitubnjq0xykmaj3yh.cx.internal.cloudapp.net>
2020-03-07 20:25:38 +0000

    Updates

---
+++ precache-manifest.8319bf7910cd7785b878126020ff7c26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f92c5d54cdeb3f10a2e9e6177fceca9e",
+    "url": "/Goorob-401-advanced-javascript/Props-and-State/index.html"
+  },
+  {
+    "revision": "c78e7e801874d1f160e0",
+    "url": "/Goorob-401-advanced-javascript/Props-and-State/static/js/2.b1b8262f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Goorob-401-advanced-javascript/Props-and-State/static/js/2.b1b8262f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9e20fb5658ab302badb",
+    "url": "/Goorob-401-advanced-javascript/Props-and-State/static/js/main.7456152e.chunk.js"
+  },
+  {
+    "revision": "408cf31da8058c49ee2d",
+    "url": "/Goorob-401-advanced-javascript/Props-and-State/static/js/runtime-main.9ad736b1.js"
+  }
+]);
