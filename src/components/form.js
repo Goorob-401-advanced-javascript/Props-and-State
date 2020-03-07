11f8d3a3d0d575f5358dce609b351ce82eb59a5c
LTUC Student 1 <std1@QUDSLABS.LOCAL>
2020-03-07 21:38:49 +0200

    edditing

--- src/components/form.js
+++ src/components/form.js
@@ -2,19 +2,80 @@
 import React from 'react';
 
 class Form extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      url: '',
-      method: 'get',
-      header: {},
-      body: {},
-      headersVisible: false,
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            url: '',
+            method: 'get',
+            header: {},
+            body: {},
+        };
+    }
 
- 
+    changeHandler = event => {
+        let name = event.target.name;
+        let value = event.target.value;
+        this.setState({ [name]: value });
+    }
+
+    getData = event => {
+        event.preventDefault();
+        fetch(this.state.url, {
+            method: this.state.method,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(response => {
+                response = response.json()
+                    .then(response => {
+                        let header = { 'Content-Type': 'application/json' };
+                        let body = response;
+                        this.setState({ header, body });
+                        this.props.info({ header, body });
+                    })
+                    .catch(err => {
+                        let body = { error: err.message };
+                        let header = {};
+                        this.setState({ header, body });
+                        this.props.info({ header, body });
+                    });
+            });
+    }
+    render() {
+        return (
+            <main>
+                <section>
+                    <form onSubmit={this.getData}>
+                        <section>
+                            <input type="text" className="url" name="url" value={this.state.url} onChange={this.changeHandler} />
+                            <div className="methods">
+                                <label>
+                                    <input type="radio" name="method" value="get" />
+                                    <span>GET</span>
+                                </label>
+                                <label>
+                                    <input type="radio" name="method" value="post" />
+                                    <span>POST</span>
+                                </label>
+                                <label>
+                                    <input type="radio" name="method" value="put" />
+                                    <span>PUT</span>
+                                </label>
+                                <label>
+                                    <input type="radio" name="method" value="delete" />
+                                    <span>DELETE</span>
+                                </label>
+                                <label> <button type="submit"> GO  ! </button> </label>
+                            </div>
+                        </section>
+                    </form>
+                </section>
+            </main>
+        );
+    }
 
 }
+
 
 export default Form;
--- src/components/results.js
+++ src/components/results.js
@@ -2,8 +2,22 @@
 import React from 'react';
 
 class Results extends React.Component {
-
- 
+    
+    resultsFun= (data) => {
+        let result = JSON.stringify(data ,null , '\t');
+        return result ;
+      }
+      
+    
+      render() {
+        return(
+          <>
+            <span>
+                {this.resultsFun(this.props.results)} 
+                </span>
+          </>
+        );
+      }
 
 
 }
